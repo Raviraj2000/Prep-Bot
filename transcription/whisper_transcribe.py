41b697553ffe0a6a1f9d475fe265058a2e70defe
Raviraj2000 <ravirajshinde2000@gmail.com>
2024-06-15 13:28:29 -0700

    Mic bug fixed

--- transcription/whisper_transcribe.py
+++ transcription/whisper_transcribe.py
@@ -26,7 +26,7 @@ def record_audio():
     keyboard.wait('r')
     with st.chat_message("assistant"):
         st.markdown("Recording started please speak into the microphone... Press 'R' again to stop.")
-    stream = sd.InputStream(samplerate=sample_rate, channels=1, dtype='float32', callback=callback, device="Enter Device Number Here")
+    stream = sd.InputStream(samplerate=sample_rate, channels=1, dtype='float32', callback=callback, device=sd.default.device[0])
     stream.start()
     keyboard.wait('r')
     stream.stop()
